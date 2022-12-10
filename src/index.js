a903ef71845e0b663c2fb0599fe4a3e84d9b06f4
Esteban <bastianruizlucero@gmail.com>
2022-12-10 18:51:37 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+console.log('Hola mundos')
+
+// TIPOS DE DATOS EN JAVA SCRIPT
+
+// STRING: 'a' "Hola"
+// Boolean: True or False.
+// integer: 1 2 3 4 5 1000 9999+
+// Null nulo
+// Number: 14621423453241 "123 " 123
+// "123" es un string  que es distinto a 123 que es un number
+// Undefined.
+// Object: Objeto
+
+
+//Definicion de variables
+//var evitar utilizar
+//let 
+//const
+
+
+//var miPrimeraVariable = 'lala'
+
+let miPrimeraVariable = 'Mi primera variable!'
+console.log(miPrimeraVariable);
